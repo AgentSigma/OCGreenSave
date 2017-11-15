d06166b1570416f839c05ca03044f332fa5090e2
Reyna <dreyna3@student.cccd.edu>
2017-11-14 16:49:17 -0800

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/dreyna3/ocgreensave/CountActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.cs273.dreyna3.ocgreensave;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CountActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_count);
+    }
+}
